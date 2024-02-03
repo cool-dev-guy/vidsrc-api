abba651680020f170272cd632cfb71471fd74ae7
cool-guy-dev <116984615+cool-dev-guy@users.noreply.github.com>
2024-02-03 22:30:53 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,15 @@
-from .vidsrcme import vidsrcme
 from .subtitle import subfetch
-from .vidsrcto import vidsrcto , handle_filemoon , handle_vidplay
+VERSION = '1.0.0'
+from .upcloud import get as upcloudget
+from .vidcloud import get as vidcloudget
+from .vidsrcme import get as vidsrcmeget
+from .vidsrcto import get as vidsrctoget
+from .smashystream import get as smashyget
+# UTILS
+def info():
+    return {
+    "project":"simple-scrape-api",
+    "note":"This api is made for educational purpouse only. This is just a simple scrapper built arround `Ciarands` vidsrc downloader/resolver.This project was only made to prevent ads and redirects caused by the `iframe`s",
+    "version": VERSION,
+    "developer":"cool-dev-guy"
+    }
