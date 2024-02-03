a337959e134feddd6df485fdff43df8314b4fb4f
cool-guy-dev <116984615+cool-dev-guy@users.noreply.github.com>
2024-02-03 22:33:15 +0530

    Create superembed.py[parts of vidsrc.me]

---
+++ models/superembed.py
@@ -0,0 +1,38 @@
+# API --- vidsrc.me --- provider
+import re
+import requests
+from bs4 import BeautifulSoup
+import httpx
+import base64
+from .decoders import hunter
+from typing import Optional, Dict, List
+def process_hunter_args(hunter_args: str) -> List:
+    hunter_args = re.search(r"^\"(.*?)\",(.*?),\"(.*?)\",(.*?),(.*?),(.*?)$", hunter_args)
+    processed_matches = list(hunter_args.groups())
+    processed_matches[0] = str(processed_matches[0])
+    processed_matches[1] = int(processed_matches[1])
+    processed_matches[2] = str(processed_matches[2])
+    processed_matches[3] = int(processed_matches[3])
+    processed_matches[4] = int(processed_matches[4])
+    processed_matches[5] = int(processed_matches[5])
+    return processed_matches
+async def handle_superembed(location,source,_seed):
+    # print("STARTS\n",source,"\nBREAKS\n",location,"\nENDS")
+    req = requests.get(location, headers={"Referer":  f"https://rcp.vidsrc.me/rcp/{source}"})
+    hunter_args = re.search(r"eval\(function\(h,u,n,t,e,r\).*?}\((.*?)\)\)", req.text)
+    processed_values = []
+    if not hunter_args:
+        return f"1308 {location}",_seed
+    processed_hunter_args = process_hunter_args(hunter_args.group(1))
+    unpacked = hunter.hunter(*processed_hunter_args)
+    subtitles = {}
+    hls_urls = re.findall(r"file:\"([^\"]*)\"", unpacked)
+    subtitle_match = re.search(r"subtitle:\"([^\"]*)\"", unpacked)
+    if subtitle_match:
+        for subtitle in subtitle_match.group(1).split(","):
+            subtitle_data = re.search(r"^\[(.*?)\](.*$)", subtitle)
+            if not subtitle_data:
+                continue
+            subtitles.update({subtitle_data.group(1): subtitle_data.group(2)})
+        # print(subtitles)
+    return hls_urls[0],subtitles
