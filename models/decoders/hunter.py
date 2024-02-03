781cac95a72f5f110ee1664cc5b523902485566d
cool-guy-dev <116984615+cool-dev-guy@users.noreply.github.com>
2024-02-03 22:36:34 +0530

    Create hunter.py

---
+++ models/decoders/hunter.py
@@ -0,0 +1,37 @@
+# THE hunter DECODER
+def hunter_def(d, e, f) -> int:
+    charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/"
+    source_base = charset[0:e]
+    target_base = charset[0:f]
+
+    reversed_input = list(d)[::-1]
+    result = 0
+
+    for power, digit in enumerate(reversed_input):
+        if digit in source_base:
+            result += source_base.index(digit) * e**power
+
+    converted_result = ""
+    while result > 0:
+        converted_result = target_base[result % f] + converted_result
+        result = (result - (result % f)) // f
+
+    return int(converted_result) or 0
+def hunter( h, u, n, t, e, r) -> str:
+        i = 0
+        result_str = ""
+        while i < len(h):
+            j = 0
+            s = ""
+            while h[i] != n[e]:
+                s += h[i]
+                i += 1
+
+            while j < len(n):
+                s = s.replace(n[j], str(j))
+                j += 1
+
+            result_str += chr(hunter_def(s, e, 10) - t)
+            i += 1
+
+        return result_str
