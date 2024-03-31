01621dfa9e199db9debb208e1b9368a0668f819e
cool-guy-dev <116984615+cool-dev-guy@users.noreply.github.com>
2024-03-31 23:18:05 +0530

    Update utils.py

--- models/utils.py
+++ models/utils.py
@@ -1,3 +1,51 @@
-# BASIC UTILS
+import httpx,base64
+from fastapi import HTTPException
+from urllib.parse import unquote
+
+BASE = 'http://localhost:8000'
+
 async def default():
     return ''
+
+async def error(err:str):
+    return {
+        "status":500,
+        "info":err,
+        "sources":[]
+    }
+
+async def decode_url(encrypted_source_url:str,VIDSRC_KEY:str):
+    standardized_input = encrypted_source_url.replace('_', '/').replace('-', '+')
+    binary_data = base64.b64decode(standardized_input)
+    encoded = bytearray(binary_data)
+    key_bytes = bytes(VIDSRC_KEY, 'utf-8')
+    j = 0
+    s = bytearray(range(256))
+
+    for i in range(256):
+      j = (j + s[i] + key_bytes[i % len(key_bytes)]) & 0xff
+      s[i], s[j] = s[j], s[i]
+
+    decoded = bytearray(len(encoded))
+    i = 0
+    k = 0
+    for index in range(len(encoded)):
+      i = (i + 1) & 0xff
+      k = (k + s[i]) & 0xff
+      s[i], s[k] = s[k], s[i]
+      t = (s[i] + s[k]) & 0xff
+      decoded[index] = encoded[index] ^ s[t]
+    decoded_text = decoded.decode('utf-8')
+    return unquote(decoded_text)
+
+async def fetch(url:str,headers:dict={},method:str="GET",data=None,redirects:bool=True):
+    async with httpx.AsyncClient(follow_redirects=redirects) as client:
+        if method=="GET":
+            response = await client.get(url,headers=headers)
+            return response
+        if method=="POST":
+            response = await client.post(url,headers=headers,data=data)
+            return response
+        else:
+            return "ERROR"
+        
