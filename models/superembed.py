6e5d4c5c96fb228a87d03416e525dafd4aed3154
cool-guy-dev <116984615+cool-dev-guy@users.noreply.github.com>
2024-03-31 23:20:41 +0530

    Update superembed.py

--- models/superembed.py
+++ models/superembed.py
@@ -1,33 +1,18 @@
-# API --- superembed --- vidsrc.me provider
-# file made by @cool-dev-guy using @Ciarands hunter extractor/resolver
 import re
-import requests
-from bs4 import BeautifulSoup
-import httpx
-import base64
-from .decoders import hunter
-from typing import Optional, Dict, List
-def process_hunter_args(hunter_args: str) -> List:
-    hunter_args = re.search(r"^\"(.*?)\",(.*?),\"(.*?)\",(.*?),(.*?),(.*?)$", hunter_args)
-    processed_matches = list(hunter_args.groups())
-    processed_matches[0] = str(processed_matches[0])
-    processed_matches[1] = int(processed_matches[1])
-    processed_matches[2] = str(processed_matches[2])
-    processed_matches[3] = int(processed_matches[3])
-    processed_matches[4] = int(processed_matches[4])
-    processed_matches[5] = int(processed_matches[5])
-    return processed_matches
-async def handle_superembed(location,source,_seed):
+from .utils import fetch
+from .decoders.hunter import *
+
+async def handle(location:str,hash:str,_seed:str):
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
     }
-    req = requests.get(location,headers=headers)
-    hunter_args = re.search(r"eval\(function\(h,u,n,t,e,r\).*?}\((.*?)\)\)", req.text)
-    processed_values = []
+    request = await fetch(location,headers=headers)
+    hunter_args = re.search(r"eval\(function\(h,u,n,t,e,r\).*?}\((.*?)\)\)", request.text)
     if not hunter_args:
-        return f"1308 {location}",_seed
-    processed_hunter_args = process_hunter_args(hunter_args.group(1))
-    unpacked = hunter.hunter(*processed_hunter_args)
+        return {"stream":'',"subtitle":[]}
+    
+    processed_hunter_args = await process_hunter_args(hunter_args.group(1))
+    unpacked = await hunter(*processed_hunter_args)
     subtitles = []
     hls_urls = re.findall(r"file:\"([^\"]*)\"", unpacked)
     subtitle_match = re.search(r"subtitle:\"([^\"]*)\"", unpacked)
@@ -37,5 +22,4 @@ async def handle_superembed(location,source,_seed):
             if not subtitle_data:
                 continue
             subtitles.append({'lang':subtitle_data.group(1),'file':subtitle_data.group(2)})
-        # print(subtitles)
-    return hls_urls[0],subtitles
+    return {"stream":hls_urls[0],"subtitle":subtitles}
