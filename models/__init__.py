d6c33199e9655afabdd42cd836207f534bae0b9c
cool-guy-dev <116984615+cool-dev-guy@users.noreply.github.com>
2024-03-31 23:22:30 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,10 @@
 from .subtitle import subfetch
-VERSION = '2.0.0'
+VERSION = '3.0.0'
 from .vidsrcme import get as vidsrcmeget
 from .vidsrcto import get as vidsrctoget
+from .utils import fetch
 # UTILS
-def info():
+async def info():
     return {
     "project":"simple-scrape-api",
     "note":"This api is made for educational purpouse only. This is just a simple scrapper built arround `Ciarands` vidsrc downloader/resolver.This project was only made to prevent ads and redirects caused by the `iframe`s",
