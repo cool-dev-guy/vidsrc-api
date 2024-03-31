826d876d9d8e18a7648c0d7c5c6c29cc1360e377
cool-guy-dev <116984615+cool-dev-guy@users.noreply.github.com>
2024-03-31 23:19:52 +0530

    Update vidsrcme.py

--- models/vidsrcme.py
+++ models/vidsrcme.py
@@ -1,76 +1,56 @@
-# VIDSRC.ME
-# file made by @cool-dev-guy using @Ciarands resolver to support fastapi.
-import requests
-import requests,asyncio,httpx
+import asyncio
 from bs4 import BeautifulSoup
 from . import vidsrcpro,superembed
-from . import subtitle
-async def vidsrcme(source,url):
-    async with httpx.AsyncClient(timeout=30.0) as client:
-        response = await client.get(f"https://rcp.vidsrc.me/rcp/{source}",headers={"Referer": url})
-        _html = BeautifulSoup(response.text, "html.parser")
+from .utils import fetch
+SOURCES = ["VidSrc PRO","Superembed"]
+async def get_source(hash:str,url:str):
+    SOURCE_REQUEST = await fetch(f"https://rcp.vidsrc.me/rcp/{hash}",headers={"Referer": url})
+    try:
+        _html = BeautifulSoup(SOURCE_REQUEST.text, "html.parser")
         _encoded = _html.find("div", {"id": "hidden"}).get("data-h") if _html.find("div", {"id": "hidden"}) else None
-        if not _encoded:return None
-        _seed = _html.find("body").get("data-i")
 
+        if not _encoded:
+            return {"stream":None,"subtitle":[]}
+        
+        _seed = _html.find("body").get("data-i")
         encoded_buffer = bytes.fromhex(_encoded);decoded = ""
         for i in range(len(encoded_buffer)):decoded += chr(encoded_buffer[i] ^ ord(_seed[i % len(_seed)]))
         decoded_url = f"https:{decoded}" if decoded.startswith("//") else decoded
         
-        response = requests.get(decoded_url, allow_redirects=False, headers={"Referer": f"https://rcp.vidsrc.me/rcp/{source}"})
+        response = await fetch(decoded_url,redirects=False, headers={"Referer": f"https://rcp.vidsrc.me/rcp/{hash}"})
         location = response.headers.get("Location")
         if location is None:
-            return 1506,_seed
-        if "playhydrax.com" in location:
-            return 1500,_seed
+            return {"stream":None,"subtitle":[]}
         if "vidsrc.stream" in location:
-            req = await client.get(location, headers={"Referer": f"https://rcp.vidsrc.me/rcp/{source}"})
-            return await vidsrcpro.handle_vidsrcpro(req,source,_seed)
-        if "2embed.cc" in location:
-            return 1500,_seed
+            return await vidsrcpro.handle(location,hash,_seed)
         if "multiembed.mov" in location:
-            return await superembed.handle_superembed(location,source,_seed)
+            return await superembed.handle(location,hash,_seed)
+    except:
+        return {"stream":None,"subtitle":[]}
+
+async def get_stream(hash:str,url:str,SOURCE_NAME:str):
+    if True:
+        RESULT = {}
+        RESULT['name'] = SOURCE_NAME
+        RESULT['data'] = await get_source(hash,url)
+        return RESULT
+    else:
+        return {"name":SOURCE_NAME,"source":'',"subtitle":[]}
+
 async def get(dbid,s=None,e=None,l='eng'):
     provider = "imdb" if ("tt" in dbid) else "tmdb"
     media = 'tv' if s is not None and e is not None else "movie"
     language = l
 
-    url = f"https://vidsrc.me/embed/{dbid}"
-    url += f"/{s}-{e}" if s and e else ''
+    # MAKE API REQUEST TO GET ID(hash)
+    id_url = f"https://vidsrc.me/embed/{dbid}" + (f"/{s}-{e}" if s and e else '')
+    id_request = await fetch(id_url)
+    _html = BeautifulSoup(id_request.text, "html.parser")
+    SOURCE_RESULTS = [{"name": attr.text, "hash": attr.get("data-hash")} for attr in _html.find_all("div", {"class": "server"}) if attr.text in SOURCES]
 
-    response = requests.get(url)
-    _html = BeautifulSoup(response.text, "html.parser")
-    sources = {attr.text: attr.get("data-hash") for attr in _html.find_all("div", {"class": "server"})}
-        
-    # REMOVE UNWANTED SOURCES
-    try:
-        del sources['VidSrc Hydrax']
-    except:
-        pass
-    try:
-        del sources['2Embed']
-    except:
-        pass
-    # RESULT SCHEMA
-    source = []
-    for item in sources.keys():source.append(sources[item])
-    if not source:return 1404,None
-    results = await asyncio.gather(
-        *[vidsrcme(s,url) for s in source]
+    # REQUEST THE SOURCE
+    SOURCE_STREAMS = await asyncio.gather(
+        *[get_stream(R.get('hash'),id_url,R.get('name')) for R in SOURCE_RESULTS]
     )
-    print(results)
-    sub_seed = results[0][1] if results[0] else 1500
-    subtitles = await subtitle.subfetch(sub_seed,language) if sub_seed!=500 else 500
-    return [{
-    "name":'VidSrcPRO',
-    "data":{
-            'file':results[0][0],
-            'sub':subtitles
-        },
-    },{
-    "name":'SuperEmbed',
-    "data":{
-            'file':results[1][0] if len(results)==2 else 1500,
-            'sub':results[1][1] if len(results)==2 else 1500
-        },
-    }]
+    
+    return SOURCE_STREAMS
