3ac79fb446301ed34af22ff75375b0c829fd56e1
cool-guy-dev <116984615+cool-dev-guy@users.noreply.github.com>
2024-03-31 23:19:00 +0530

    Update vidsrcpro.py

--- models/vidsrcpro.py
+++ models/vidsrcpro.py
@@ -1,20 +1,23 @@
-# API --- vidsrc.me --- provider
-import requests,re,base64
-async def handle_vidsrcpro(req,source,_seed):
-    hls_url = re.search(r'file:"([^"]*)"', req.text).group(1)
-    hls_url = re.sub(r'\/\/\S+?=', '', hls_url)[2:]
-            
-    MAX_TRIES = 5
-    for i in range(MAX_TRIES):
-        hls_url = re.sub(r"\/@#@\/[^=\/]+==", "", hls_url)
-        if re.search(r"\/@#@\/[^=\/]+==", hls_url):
-            continue
+import re,base64
+from .utils import fetch
+from . import subtitle
+async def handle(location,hash,_seed):
+    request = await fetch(location, headers={"Referer": f"https://rcp.vidsrc.me/rcp/{hash}"})
+    hls_url = re.search(r'file:"([^"]*)"', request.text).group(1)
+    hls_url = re.sub(r'\/\/\S+?=', '', hls_url)[2:]     
+    hls_url = re.sub(r"\/@#@\/[^=\/]+==", "", hls_url)
+
     hls_url = hls_url.replace('_', '/').replace('-', '+')
     hls_url = bytearray(base64.b64decode(hls_url))
     hls_url = hls_url.decode('utf-8')
             
-    set_pass = re.search(r'var pass_path = "(.*?)";', req.text).group(1)
+    # SET PASS
+    set_pass = re.search(r'var pass_path = "(.*?)";', request.text).group(1)
     if set_pass.startswith("//"):
         set_pass = f"https:{set_pass}"
-    requests.get(set_pass, headers={"Referer": source})
-    return (hls_url),_seed
+    await fetch(set_pass, headers={"Referer": hash})
+
+    subtitles = []
+    subtitles = await subtitle.subfetch(_seed,"eng")
+    return {"stream":hls_url,"subtitle":subtitles}
+    
